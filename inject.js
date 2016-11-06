2445f0d71b8503e0f0809c7c54be9968c48c9dc6
rycao18 <rycao18@gmail.com>
2016-11-06 04:31:09 +0000

    1131pm commit

--- inject.js
+++ inject.js
@@ -3,9 +3,12 @@
     var passwordBox;
     var inputs = document.getElementsByTagName("input");
     for(var i = 0; i < inputs.length; i++){
-        if (inputs[i].type == "password") {
+        if (inputs[i].attr('type') == "password") {
             usernameBox = inputs[i - 1];
             passwordBox = inputs[i];
         }
     }
+    console.log(usernameBox);
+    console.log(passwordBox);
+    console.log(inputs);
 })();
